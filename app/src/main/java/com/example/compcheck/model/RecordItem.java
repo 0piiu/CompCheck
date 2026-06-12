5fc9324fdba54bfb794efb23a4458381b20181f9
Thales Santos <thaleszezun@gmail.com>
2026-06-12 19:35:36 -0300

    Terminado todas as entidades, vamos para o proximo passo!

---
+++ app/src/main/java/com/example/compcheck/model/RecordItem.java
@@ -0,0 +1,80 @@
+package com.example.compcheck.model;
+
+
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+@Entity(
+        tableName = "record_items",
+        foreignKeys = {
+                @ForeignKey(
+                        entity = Record.class,
+                        parentColumns = "id",
+                        childColumns = "recordId",
+                        onDelete = ForeignKey.CASCADE
+                ),
+                @ForeignKey(
+                        entity = TemplateItem.class,
+                        parentColumns = "id",
+                        childColumns = "templateItemId",
+                        onDelete = ForeignKey.CASCADE
+                )
+        }
+
+)
+public class RecordItem {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private int recordId;
+    private int templateItemId;
+    private String status;
+    private String observation;
+
+    public RecordItem(int recordId, int templateItemId, String status, String observation) {
+        this.recordId = recordId;
+        this.templateItemId = templateItemId;
+        this.status = status;
+        this.observation = observation;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setRecordId(int recordId) {
+        this.recordId = recordId;
+    }
+
+    public void setTemplateItemId(int templateItemId) {
+        this.templateItemId = templateItemId;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setObservation(String observation) {
+        this.observation = observation;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getRecordId() {
+        return recordId;
+    }
+
+    public int getTemplateItemId() {
+        return templateItemId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getObservation() {
+        return observation;
+    }
+}
